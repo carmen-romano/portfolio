53d9294e2b631260c5b95115e95b4c9c2bebf02e
Carmen Romano <my-9@hotmail.it>
2024-06-02 22:14:24 +0200

    add skill component

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,12 @@
 import "./App.css";
 import "bootstrap/dist/css/bootstrap.min.css";
 import MyNavbar from "./components/Navbar";
-import { BrowserRouter, Route, Routes } from "react-router-dom";
 import Project from "./pages/Project";
 import Contact from "./pages/Contact";
 import Home from "./pages/Home";
 import { useState } from "react";
+import Skills from "./components/Skills";
+import { Container } from "react-bootstrap";
 
 function App() {
   const [isLoading, setIsLoading] = useState(true);
@@ -20,14 +21,15 @@ function App() {
   return (
     <>
       {!isLoading && (
-        <BrowserRouter>
+        <>
           <MyNavbar />
-          <Routes>
-            <Route path="/" element={<Home />} />
-            <Route path="/project" element={<Project />} />
-            <Route path="/contact" element={<Contact />} />
-          </Routes>
-        </BrowserRouter>
+          <Container>
+            <Home />
+            <Project />
+            <Contact />
+            <Skills />
+          </Container>
+        </>
       )}
     </>
   );
--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,31 +1,39 @@
 import { Container, Nav, Navbar } from "react-bootstrap";
-import { NavLink } from "react-router-dom";
 import "../style/navbar.css";
 import Circle from "./Circle";
 
 const MyNavbar = () => {
   return (
-    <Navbar className="bg-body-tertiary" expand={false} id="navbar">
+    <Navbar
+      collapseOnSelect
+      expand="lg"
+      bg="light"
+      data-bs-theme="light"
+      id="navbar"
+    >
       <Container>
-        <Navbar.Brand to={"/"}>
-          <p className="title fs-6">
-            <span className="symbol">{"<"}</span> Carmen Romano
-            <span className="symbol mx-2">{"/>"}</span>
-          </p>
+        <Navbar.Brand href="#home">
+          <div>
+            <p className="title fs-6">
+              <span className="symbol">{"<"}</span> Carmen Romano
+              <span className="symbol mx-2">{"/>"}</span>
+            </p>
+          </div>
         </Navbar.Brand>
-        <NavLink to={"/"}>
-          <Circle />
-        </NavLink>
-        <Navbar.Toggle
-          aria-controls="basic-navbar-nav"
-          className="custom-toggler fs-6"
-        >
-          Menu
-        </Navbar.Toggle>
-        <Navbar.Collapse id="basic-navbar-nav">
-          <Nav className="me-auto d-flex">
-            <NavLink to={"/project"}>PROJECT</NavLink>
-            <NavLink to={"/contact"}>CONTACT</NavLink>
+        <Navbar.Toggle aria-controls="responsive-navbar-nav" />
+        <Navbar.Collapse id="responsive-navbar-nav">
+          <Nav className="me-auto">
+            <Nav.Link href="#features">
+              <Circle />
+            </Nav.Link>
+          </Nav>
+          <Nav>
+            <Nav.Link href="#deets" className="title">
+              PROJECT
+            </Nav.Link>
+            <Nav.Link href="#memes" className="title">
+              CONTACT
+            </Nav.Link>
           </Nav>
         </Navbar.Collapse>
       </Container>
