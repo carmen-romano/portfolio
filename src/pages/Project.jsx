92135f26a36a8cb64fa70f94e1ec9ac23639654e
Carmen Romano <my-9@hotmail.it>
2024-06-04 21:56:19 +0200

    project section add component

--- src/pages/Project.jsx
+++ src/pages/Project.jsx
@@ -1,32 +1,71 @@
 import { Col, Container, Image, Row } from "react-bootstrap";
 import "../style/project.css";
+import appMeteo from "../../public/images/project/appmeather.webp";
+import spotify from "../../public/images/project/spotifyClone.webp";
+import linkedin from "../../public/images/project/linkedinclone.webp";
+
+import "../style/skillCarousel.css";
 
 let Project = () => {
+  const project = [
+    {
+      name: "Spotify Clone",
+      project: spotify,
+      description:
+        "Lorem ipsum dolor sit amet consectetur adipisicing elit. Et",
+      linearGradient:
+        "linear-gradient(to bottom, #25bf5bd0 30%, #3da662c5 100%)",
+      linearGradient2:
+        "linear-gradient(to bottom, #41cf7242 0%, #1d62356e 40%)",
+    },
+    {
+      name: "LinkedIn Clone",
+      project: linkedin,
+      description:
+        "Lorem ipsum dolor sit amet consectetur adipisicing elit. Et",
+      linearGradient:
+        "linear-gradient(to bottom, #1068c1bf 30%, #3772ad  100%)",
+      linearGradient2:
+        "linear-gradient(to bottom, #3d8ad7  0%, #0a66c28d  40%)",
+    },
+    {
+      name: "App Meteo",
+      project: appMeteo,
+      description:
+        "Lorem ipsum dolor sit amet consectetur adipisicing elit. Et",
+      linearGradient: "linear-gradient(to bottom, #bdeefe 30%, #2ab9e9c7 100%)",
+      linearGradient2:
+        "linear-gradient(to bottom, #a9e9ff6e 0%, #41abcf95 40%)",
+    },
+  ];
+
   return (
     <Container id="project" className="my-5">
       <h2 className="text-center">Progetti</h2>
       <Row>
-        <Col>
-          <div className="wrapper">
-            <div className="container" id="c0">
-              <div className="image">
-                <Image
-                  src="/public/images/project/spotifyClone.webp"
-                  className="w-100 mt-3 rounded"
-                />
-              </div>
-              <div className="story" id="s0">
-                <h3>Spotify Clone </h3>
-                <p>
-                  Lorem ipsum dolor sit amet consectetur adipisicing elit. Et
-                  dicta repellendus atque quae laboriosam reiciendis eveniet
-                  maxime delectus voluptatem rerum veritatis optio, odio
-                  corrupti iure qui, non eum, perferendis eligendi.
-                </p>
+        {project.map((project, id) => (
+          <Col key={id}>
+            <div className="wrapper">
+              <div
+                className="container"
+                id="c0"
+                style={{ background: project.linearGradient }}
+              >
+                <div className="image">
+                  <Image src={project.project} className="w-100 mt-3 rounded" />
+                </div>
+                <div
+                  className="story"
+                  id="s0"
+                  style={{ background: project.linearGradient2 }}
+                >
+                  <h3>{project.name} </h3>
+                  <p>{project.description}</p>
+                </div>
               </div>
             </div>
-          </div>
-        </Col>
+          </Col>
+        ))}
       </Row>
     </Container>
   );
