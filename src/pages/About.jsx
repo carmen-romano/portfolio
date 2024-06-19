0ca2d2e2cc6655874cf2d011a04906fa251a0cbe
Carmen Romano <my-9@hotmail.it>
2024-06-19 17:46:08 +0200

    Vari fix

--- src/pages/About.jsx
+++ src/pages/About.jsx
@@ -1,33 +1,51 @@
 import "../style/about.css";
-import { BiSolidHandDown } from "react-icons/bi";
-import { Col, Container, Row } from "react-bootstrap";
+import { Col, Container, Nav, Row } from "react-bootstrap";
+import { FaArrowDownLong } from "react-icons/fa6";
 
 let About = () => {
   return (
     <div id="about">
       <Container>
         <Row>
+          <hr />
+          <div className="my-5">
+            <h2 className="text-center ">Qualcosa su di me</h2>
+          </div>
           <Col>
-            <div className="my-5">
-              <h2 className="text-center">
-                Qualcosa su di me
-                <BiSolidHandDown className="fs-4 mx-3" />
+            <div>
+              <p className="fs-4">
+                Dopo aver coltivato la mia passione per la programmazione
+                attraverso lo studio autonomo, ho deciso di intraprendere un
+                corso intensivo di formazione presso Epicode per ampliare e
+                consolidare le mie conoscenze nel mondo dello sviluppo web. Le
+                mie esperienze lavorative precedenti nel settore vinicolo e in
+                altre sfide professionali mi hanno fornito una solida base di
+                competenze trasversali, tra cui capacità di{" "}
+                <span className="fw-bold">problem solving</span>, gestione del
+                tempo e team working. Se vuoi saperne di più sulle mie
+                esperienze nello sviluppo o sei interessato a una
+                collaborazione,
+                <Nav.Link
+                  href="https://www.linkedin.com/in/carmen-romano-44aa6998/"
+                  target="_blank"
+                  rel="noopener noreferrer"
+                  className=" mx-2 fw-bold d-inline-block"
+                >
+                  colleghiamoci!
+                </Nav.Link>
+              </p>
+              <h2 className="fs-5 fw-light">
+                <a
+                  href="/path/to/curriculum-vitae.pdf"
+                  download="Curriculum_Vitae_Carmen_Romano.pdf"
+                  className="text-decoration-none text-dark"
+                >
+                  {/* DA FARE  */}
+                  <FaArrowDownLong className="fs-5 mx-2" />
+                  Curriculum Vitae
+                </a>
               </h2>
             </div>
-
-            <p className="fs-4">
-              Dopo aver coltivato la mia passione per la programmazione
-              attraverso lo studio autonomo, ho deciso di intraprendere un corso
-              intensivo di formazione presso Epicode per ampliare e consolidare
-              le mie conoscenze nel mondo dello sviluppo web. Le mie esperienze
-              lavorative precedenti nel settore vinicolo e in altre sfide
-              professionali mi hanno fornito una solida base di competenze
-              trasversali, tra cui capacità di{" "}
-              <span className="fw-bold">problem solving</span>, gestione del
-              tempo e collaborazione efficace. Se vuoi saperne di più sulle mie
-              esperienze nello sviluppo o sei interessato a una collaborazione,
-              <span className="fw-bold"> colleghiamoci!</span>
-            </p>
           </Col>
         </Row>
       </Container>
--- src/pages/Project.jsx
+++ src/pages/Project.jsx
@@ -5,7 +5,6 @@ import spotify from "../../public/images/project/spotifyClone.webp";
 import linkedin from "../../public/images/project/linkedinclone.webp";
 
 import "../style/skillCarousel.css";
-import Skills from "../components/Skills";
 
 let Project = () => {
   const project = [
@@ -14,29 +13,18 @@ let Project = () => {
       project: spotify,
       description:
         "'Spotify Clone' realizzato interamente in JS Vanilla, offre funzionalità di ricerca, riproduzione e gestione di brani musicali e esplorazione di album e artisti, con un'interfaccia intuitiva e reattiva.",
-      linearGradient:
-        "linear-gradient(to bottom, #25bf5bd0 30%, #3da662c5 100%)",
-      linearGradient2:
-        "linear-gradient(to bottom, #41cf7242 0%, #1d62356e 40%)",
     },
     {
       name: "LinkedIn Clone",
       project: linkedin,
       description:
         "'LinkedIn Clone' consente agli utenti di creare profili, connettersi, condividere post e interagire con il contenuto. Utilizza React per l'interfaccia utente, Redux per lo stato e un'API personalizzata per la gestione dei dati.",
-      linearGradient:
-        "linear-gradient(to bottom, #1068c1bf 30%, #3772ad  100%)",
-      linearGradient2:
-        "linear-gradient(to bottom, #3d8ad7  0%, #0a66c28d  40%)",
     },
     {
       name: "App Meteo",
       project: appMeteo,
       description:
         "Questo progetto utilizza l'API di OpenWeather per visualizzare le previsioni meteorologiche. Implementato con React, offre agli utenti informazioni meteorologiche attuali e future in modo intuitivo.",
-      linearGradient: "linear-gradient(to bottom, #bdeefe 30%, #2ab9e9c7 100%)",
-      linearGradient2:
-        "linear-gradient(to bottom, #a9e9ff6e 0%, #41abcf95 40%)",
     },
   ];
 
@@ -50,11 +38,7 @@ let Project = () => {
             className="d-flex-align-content-center element-to-watch"
           >
             <div className="wrapper">
-              <div
-                className="container"
-                id="c0"
-                style={{ background: project.linearGradient }}
-              >
+              <div className="container" id="c0">
                 <div className="image">
                   <Image src={project.project} className="w-100 mt-3 rounded" />
                 </div>
